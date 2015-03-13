53eb5948c95eb1354da39619f270f9bcdbca481d
vieplivee <vieplivee@gmail.com>
2015-03-12 21:08:54 -0400

    add files

--- README.md
+++ README.md
@@ -4,44 +4,28 @@
 
 ### What is Data Science
 
-- Skills and Domain Expertise
-- Skills you need to do data science
-    - Programming
-        - Python
-        - R
-        - SQL
-    - Statistics
-        - Probability Theory
-        - Descriptive Statistics
-        - Machine Learning
-    - Visualization
-- Domain Expertise guides how you do data science
-    - Data Munging
-        - ETL
-        - Data Cleansing
-        - Debugging
-    - Data Mining
-        - Data Exploration
-        - Machine Learning
-        - Model Evaluation
-    - Actionable Insights
+<img src="graphs/data_science_skills_domain.png" width="500"/>
 
 ----
 
 ### Data Science in Action
 
 - Customer churn model
+    - [Churn Model Demo in R](src/churn.R)
 
 ----
 
-### References
+### Data Munging Toolbox
 
-- Programming Languages and Software
-    - Programming languages
-        - Python
-        - R
-        - SQL
-    - Software
+<img src="graphs/data_tools.png" width="500"/>
 
-- Big Data
-- Books
+----
+
+### Data Visualization Toolbox
+
+<img src="graphs/data_visualization_tools.png" width="500"/>
+
+----
+
+- Created: 03.12.2015
+- Last Updated: 03.12.2015
--- src/churn.R
+++ src/churn.R
@@ -1,6 +1,8 @@
 ##################################################################################
+### Step 1: prepare space                                                      ###
 
-rm(list = ls(all = TRUE)) # clean up space
+# clean up space
+rm(list = ls(all = TRUE))
 
 # load packages
 packages <- c('data.table', 'plyr', 'dplyr', 'reshape2', 'stringr', # general
@@ -8,39 +10,45 @@ packages <- c('data.table', 'plyr', 'dplyr', 'reshape2', 'stringr', # general
               'pROC', 'ROCR', # ROC curves
               'caret', 'randomForest', 'unbalanced' # modeling
 )
-lapply(packages, require, character.only = T);
-rm(packages) # clean-up
+lapply(packages, require, character.only = T)
+
+# clean-up
+rm(packages)
 
 ##################################################################################
+### Step 2: read data and quickly explore                                      ###
 
 # reproducible example
 set.seed(1)
 
-# path
-path <- "/Users/jilitheyoda/Dropbox/Ji_Academic/DataScienct_Talk/Demo_CustomerChurnModel/"
-setwd(path)
+# read data
+rawdata <- read.csv("data/churn/churn_fix.csv", header = T)
 
-##################################################################################
+# check data
+str(rawdata)
 
-rawdata <- read.csv(str_c(path, "churn_fix.csv"), header = T)
-
-#########################################
-
+# create a working copy of the raw data
 data <- rawdata
 
 #########################################
-
 # correlation matrix
+
+# creating a copy for transforming columns to numeric
 num_data <- data
-vars <- colnames(data)
-catVars <- vars[sapply(data[, vars], class) %in% c('factor','character')]
+vars <- colnames(num_data)
+catVars <- vars[sapply(num_data[, vars], class) %in% c('factor','character')]
 for(v in catVars) {
-  num_data[,v] <- as.numeric(as.factor(num_data[,v]))
+  num_data[, v] <- as.numeric(as.factor(num_data[,v]))
 }
 
+# correlation data
 cor_data <- melt(cor(num_data))
 names(cor_data) <- c('v1', 'v2', 'correlation')
 cor_data <- mutate(cor_data, correlation = round(correlation, 2))
+str(cor_data)
+head(cor_data)
+
+# plot correlation
 ggplot(cor_data, aes(v1, v2, fill = correlation, label = correlation)) + 
   geom_tile() +
   geom_text(colour = "white") +
@@ -48,48 +56,73 @@ ggplot(cor_data, aes(v1, v2, fill = correlation, label = correlation)) +
   xlab("") + ylab("") + 
   ggtitle("Correlation Matrix")
 
-#########################################
+# clean up
+rm(num_data, cor_data, catVars, v, vars)
 
+#########################################
 # get state groups
+
 data$state <- as.character(data$state)
 a <- as.data.frame(table(data$state))
 states_select <- as.vector(a[a$Freq > 65, ][[1]])
 data$state_group <- ifelse(data$state %in% states_select, data$state, "other")
 
-#########################################
+state_col <- c("state", "state_group")
+for(v in state_col) {
+  data[,v] <- as.factor(data[,v])
+}
 
-# remove un-related columns
+# check result
+str(data[, state_col])
+
+# clean up
+rm(a, v, states_select, state_col)
+
+#########################################
+# remove un-related and redundant columns
+
 cols_remove <- c(
   "customer_id", "days_renew", "state", 
   "area_code", "phone", "is_vmail_plan",
   "day_charge", "eve_charge", "night_charge")
 data <- data[, setdiff(colnames(data), cols_remove)]
 
-#########################################
+# clean up
+rm(cols_remove)
 
+#########################################
 # get average mins per call
+
 data$day_avg_mins <- ifelse(data$day_calls == 0, -1, data$day_mins / data$day_calls)
 data$eve_avg_mins <- ifelse(data$eve_calls == 0, -1, data$eve_mins / data$eve_calls)
 data$night_avg_mins <- ifelse(data$night_calls == 0, -1, data$night_mins / data$night_calls)
 
 #########################################
-
 # convert character to factor
+
 vars <- colnames(data)
-catVars <- vars[sapply(data[, vars], class) %in% c('factor','character')]
+catVars <- vars[sapply(data[, vars], class) %in% c('character')]
 for(v in catVars) {
   data[,v] <- as.factor(data[,v])
 }
 
+# check data
+str(data)
+
+# clean up
+rm(vars, catVars, v)
+
 #########################################
 
-# check missing ata
-data[!complete.cases(data),]
+# check missing data
+if (nrow(data[!complete.cases(data),]) > 0) {
+  print("There is missing data!!!")
+}
 
 ##################################################################################
+### Step 3: random forest                                                      ###
 
-# split data
-
+# first, we need to split data into training and testing
 splitdf <- function(dataframe, seed = NULL, ratio = 0.5) {
   if (!is.null(seed))
     set.seed(seed)
@@ -99,30 +132,37 @@ splitdf <- function(dataframe, seed = NULL, ratio = 0.5) {
   testset <- dataframe[-trainindex, ]
   list(trainset=trainset,testset=testset)
 }
-getseed = as.integer(runif(1, 1, 100000)); print(paste("Seeding for split:", getseed))
-splits <- splitdf(data, seed = getseed, ratio = 1/2)
+getseed = as.integer(runif(1, 1, 100000))
+print(paste("Seeding for split:", getseed))
+
+# split data by 2:1 ratio since the data set is small
+splits <- splitdf(data, seed = getseed, ratio = 2/3)
 lapply(splits, nrow)
 lapply(splits, head)
 training <- splits$trainset
 testing <- splits$testset
 
-#########################################
+str(training)
+str(testing)
 
-# plant a random forest
+# clean up
+rm(getseed, splits, splitdf)
+
+#########################################
+# Run Random Forest
+
 model <- randomForest(
   is_churn~.
   ,data = training
   ,ntree = 100
-  ,mtry = 5
+  ,mtry = 5 # take square root of number of features
   ,replace = T
   ,importance = T
-  ,do.trace = T
+  # ,do.trace = T #watch OOB
 )
 
-varImp(model)
-varImpPlot(model, type=1, n.var = 10, main = "Random Forest")
-
 #########################################
+# Model evaluation: ROC
 
 target = "is_churn"
 
@@ -133,17 +173,31 @@ training_predicted <- predict(
 testing_actual <- testing[, grep(target, colnames(testing))]
 testing_predicted <- predict(
   model, newdata = testing[ , - grep(target, colnames(testing))], type = "prob")
+
 par(mfrow = c(1,2))
+par(pty = "s") # square plotting region
 plot.roc(training_actual, training_predicted[, 2], col = "blue", print.auc = T)
 plot.roc(testing_actual, testing_predicted[, 2], col = "red", print.auc = T)
 par(mfrow = c(1,1))
 
-#########################################
+# clean up
+rm(training_actual, training_predicted,
+   testing_actual, testing_predicted, target,
+   testing, training)
 
-# optimize the cutoff for testing data
-pred <- prediction(testing_predicted[, 2], testing_actual)
-perf <- performance(pred, measure="acc", x.measure="cutoff")
-bestAccInd <- which.max(perf@"y.values"[[1]])
-bestMsg <- print(paste("best accuracy = ", perf@"y.values"[[1]][bestAccInd],
-                       " at cutoff = ", round(perf@"x.values"[[1]][bestAccInd],4), sep=""))
-plot(perf, type = "b", sub = bestMsg)
+#########################################
+# Model evaluation: Top Features
+
+par(mfrow = c(1,2))
+par(pty = "s") # square plotting region
+varImpPlot(
+  model
+  ,type = 1 # mean decrease in accuracy
+  ,n.var = 10
+  ,main = "Top Features Type 1")
+varImpPlot(
+  model
+  ,type = 2 # mean decrease in node impurity
+  ,n.var = 10
+  ,main = "Top Features Type 2")
+par(mfrow = c(1,1))
