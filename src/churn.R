e8b9aac220b1e5aa8600900e99a126ad63b1c9f4
vieplivee <vieplivee@gmail.com>
2015-03-24 01:40:43 -0400

    fix presentation

--- src/churn.R
+++ src/churn.R
@@ -5,12 +5,7 @@
 rm(list = ls(all = TRUE))
 
 # load packages
-packages <- c(
-  'data.table', 'plyr', 'dplyr', 'reshape2', 'stringr', # general
-  'ggplot2', 'lattice', 'lubridate', # plotting and dates
-  'pROC', 'ROCR', # ROC curves
-  'caret', 'randomForest', 'unbalanced' # modeling
-  )
+packages <- c('stringr','reshape2','ggplot2','dplyr','pROC','caret','randomForest')
 lapply(packages, require, character.only = T)
 
 # clean-up
@@ -31,33 +26,26 @@ data <- rawdata
 #########################################
 # Feature Construction: average mins per call
 
-data$day_avg_mins <- ifelse(
-  data$day_calls == 0, -1, data$day_mins / data$day_calls
-  )
-data$eve_avg_mins <- ifelse(
-  data$eve_calls == 0, -1, data$eve_mins / data$eve_calls
-  )
-data$night_avg_mins <- ifelse(
-  data$night_calls == 0, -1, data$night_mins / data$night_calls
-  )
+data$day_avg_mins <- ifelse(data$day_calls == 0, -1, data$day_mins / data$day_calls)
+data$eve_avg_mins <- ifelse(data$eve_calls == 0, -1, data$eve_mins / data$eve_calls)
+data$night_avg_mins <- ifelse(data$night_calls == 0, -1, data$night_mins / data$night_calls)
 
 #########################################
 # Correlation Matrix
 
 # creating a copy for transforming columns to numeric
 num_data <- data; vars <- colnames(num_data)
-catVars <- vars[
-  sapply(num_data[, vars], class) %in% c('factor','character')
-  ]
-for(v in catVars) {
-  num_data[, v] <- as.numeric(as.factor(num_data[,v]))
-  }
+catVars <- vars[sapply(num_data[, vars], class) %in% c('factor','character')]
+for(v in catVars) {num_data[, v] <- as.numeric(as.factor(num_data[,v]))}
 
 # correlation data
 cor_data <- melt(cor(num_data))
 names(cor_data) <- c('v1', 'v2', 'correlation')
-cor_data <- mutate(cor_data, correlation = round(correlation, 2))
-str(cor_data); head(cor_data) # check correlation data
+cor_data <- mutate(cor_data, correlation = round(correlation, 1))
+
+# check correlation data
+str(cor_data)
+head(cor_data)
 
 # plot correlation
 ggplot(cor_data, aes(v1, v2, fill = correlation, label = correlation)) + 
@@ -73,6 +61,9 @@ rm(num_data, cor_data, catVars, v, vars)
 #########################################
 # get state groups
 
+# construct a new column state_group
+# in which states with higher frequencies are kept
+# and states with lower frequencies are grouped to "other"
 data$state <- as.character(data$state)
 a <- as.data.frame(table(data$state))
 states_select <- as.vector(a[a$Freq > 65, ][[1]])
@@ -82,13 +73,13 @@ data$state_group <- ifelse(
   "other"
   )
 
+# change the state columns to factor
 state_col <- c("state", "state_group")
-for(v in state_col) {
-  data[,v] <- as.factor(data[,v])
-}
+for(v in state_col) {data[,v] <- as.factor(data[,v])}
 
 # check result
 str(data[, state_col])
+head(data[, state_col])
 
 # clean up
 rm(a, v, states_select, state_col)
@@ -110,12 +101,8 @@ rm(cols_remove)
 # convert character to factor
 
 vars <- colnames(data)
-catVars <- vars[
-  sapply(data[, vars], class) %in% c('character')
-  ]
-for(v in catVars) {
-  data[,v] <- as.factor(data[,v])
-}
+catVars <- vars[sapply(data[, vars], class) %in% c('character')]
+for(v in catVars) {data[,v] <- as.factor(data[,v])}
 
 # check data
 str(data)
@@ -126,9 +113,7 @@ rm(vars, catVars, v)
 #########################################
 
 # check missing data
-if (nrow(data[!complete.cases(data),]) > 0) {
-  print("There is missing data!!!")
-}
+if (nrow(data[!complete.cases(data),]) > 0) {print("There is missing data!!!")}
 
 ##################################################################################
 ### Step 3: random forest                                                      ###
@@ -143,16 +128,18 @@ splitdf <- function(dataframe, seed = NULL, ratio = 0.5) {
   testset <- dataframe[-trainindex, ]
   list(trainset=trainset,testset=testset)
 }
-getseed = as.integer(runif(1, 1, 100000)); # random seeding
-print(paste("Seeding for split:", getseed)) # record the seed for reproducibility
+
+# random seeding
+getseed = as.integer(runif(1, 1, 100000))
+# record the seed for reproducibility
+print(paste("Seeding for split:", getseed))
 
 # split data by 2:1 ratio since the data set is small
 splits <- splitdf(data, seed = getseed, ratio = 2/3)
-lapply(splits, nrow)
-lapply(splits, head)
 training <- splits$trainset
 testing <- splits$testset
 
+# check training and testing data
 str(training)
 str(testing)
 
@@ -169,24 +156,27 @@ model <- randomForest(
   ,mtry = 5 # take square root of number of features
   ,replace = T
   ,importance = T
-  # ,do.trace = T #watch OOB
 )
 
 #########################################
 # Model evaluation: ROC
 
 target = "is_churn"
-train_target = grep(target, colnames(training))
-test_target = grep(target, colnames(testing))
+target_col = grep(target, colnames(training))
 
 # plot ROC curves for training and testing data
-training_actual <- training[, train_target]
+training_actual <- training[, target_col]
 training_predicted <- predict(
-  model, newdata = training[ , - train_target], type = "prob")
-testing_actual <- testing[, test_target]
+  model, newdata = training[ , - target_col], type = "prob")
+testing_actual <- testing[, target_col]
 testing_predicted <- predict(
-  model, newdata = testing[ , - test_target], type = "prob")
+  model, newdata = testing[ , - target_col], type = "prob")
 
+# check propensity scores on the testing data
+str(testing_predicted)
+head(testing_predicted)
+
+# plot ROC
 par(mfrow = c(1,2))
 par(pty = "s") # square plotting region
 plot.roc(training_actual, training_predicted[, 2], col = "blue", print.auc = T)
@@ -201,17 +191,9 @@ rm(training_actual, training_predicted,
 #########################################
 # Model evaluation: Top Features
 
-par(mfrow = c(1,2))
 par(pty = "s") # square plotting region
 varImpPlot(
   model
   ,type = 1 # mean decrease in accuracy
   ,n.var = 10
-  ,main = "Top Features Type 1")
-varImpPlot(
-  model
-  ,type = 2 # mean decrease in node impurity
-  ,n.var = 10
-  ,main = "Top Features Type 2")
-par(mfrow = c(1,1))
-
+  ,main = "Top Features")
